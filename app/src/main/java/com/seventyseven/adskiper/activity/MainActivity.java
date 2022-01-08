c609eaf689e8d321ead45e3a746fd5a8ef4dd90f
微风的火原 <1075969266@qq.com>
2022-01-09 00:19:07 +0800

    chang 1

--- app/src/main/java/com/seventyseven/adskiper/activity/MainActivity.java
+++ app/src/main/java/com/seventyseven/adskiper/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.seventyseven.adskiper.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -21,9 +23,11 @@ import com.seventyseven.adskiper.R;
 import com.seventyseven.adskiper.service.SkipService;
 import com.suke.widget.SwitchButton;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Intent intent;
+    private Intent intent = new Intent(MainActivity.this, SkipService.class);;
     private TextView ranktv;
     private TextView serviceState;
     private com.suke.widget.SwitchButton switchButton;
@@ -65,10 +69,24 @@ public class MainActivity extends AppCompatActivity {
                         };
                         //延迟跳转打开无障碍设置
                         handler.postDelayed(runnable,1500);
+                        //启动服务
+                        startService(intent);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                serviceState.setText("服务已开启");
+                            }
+                        });
+                        Toast.makeText(getApplicationContext(), "服务已开启", Toast.LENGTH_SHORT).show();
                     } else {
                         //启动服务
-                        intent = new Intent(MainActivity.this, SkipService.class);
                         startService(intent);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                serviceState.setText("服务已开启");
+                            }
+                        });
                         Toast.makeText(getApplicationContext(), "服务已开启", Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -76,8 +94,13 @@ public class MainActivity extends AppCompatActivity {
                     //关闭服务
                     stopService(intent);
                     Toast.makeText(getApplicationContext(), "服务已关闭", Toast.LENGTH_SHORT).show();
-                    switchButton.setChecked(false);
-                    serviceState.setText("服务已关闭");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            serviceState.setText("服务已关闭");
+                        }
+                    });
+
                 }
             }
         });
@@ -96,25 +119,9 @@ public class MainActivity extends AppCompatActivity {
         } catch (Settings.SettingNotFoundException e) {
             Log.e(TAG, "Error finding setting, default accessibility to not found: " + e.getMessage());
         }
-
-        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
-
         if (accessibilityEnabled == 1) {
             Log.d(TAG, "***ACCESSIBILITY IS ENABLED*** -----------------");
-            serviceState.setText("服务已开启");
-            String settingValue = Settings.Secure.getString(mContext.getApplicationContext().getContentResolver(),
-                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
-            if (settingValue != null) {
-                mStringColonSplitter.setString(settingValue);
-                while (mStringColonSplitter.hasNext()) {
-                    String accessibilityService = mStringColonSplitter.next();
-                    Log.d(TAG, "-------------- > accessibilityService :: " + accessibilityService + " " + service);
-                    if (accessibilityService.equalsIgnoreCase(service)) {
-                        Log.d(TAG, "We've found the correct setting - accessibility is switched on!");
-                        return true;
-                    }
-                }
-            }
+            return true;
         } else {
             Log.d(TAG, "***ACCESSIBILITY IS DISABLED***");
             serviceState.setText("服务已关闭");
@@ -125,14 +132,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(!isAccessibilityOpen(MainActivity.this, SkipService.class.getCanonicalName())){
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    switchButton.setChecked(false);
-                }
-            });
-        }
     }
 
     class MygestureListener extends GestureDetector.SimpleOnGestureListener{
